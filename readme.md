57ebec7ef5340063277dfd5dbc47db327e13de6d
benrobo <alumonabenaiah71@gmail.com>
2021-04-13 02:31:19 -0700

    first commit

---
+++ readme.md
@@ -0,0 +1,34 @@
+## Site Tracker.
+
+<img src="https://github.com/Benrobo/Site-Tracker/blob/main/ghnfgn.PNG">
+
+## This is just a simple based web version for tracking visitor who visited your site using 
+
+-- Ajax
+-- Javascript
+-- PHP
+-- Mysql
+
+
+### Before starting out or running this script make sure you run this sql statement below:
+
+<pre><code>
+--
+-- Table structure for table `views`
+--
+
+CREATE TABLE `views` (
+  `id` int(12) NOT NULL,
+  `count` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `views`
+--
+
+INSERT INTO `views` (`id`, `count`) VALUES
+(1, 2),
+(2, 2);
+</code></pre>
+
+#### You're all setup.
---
+++ views.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 13, 2021 at 11:29 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `views`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `views`
+--
+
+CREATE TABLE `views` (
+  `id` int(12) NOT NULL,
+  `count` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `views`
+--
+
+INSERT INTO `views` (`id`, `count`) VALUES
+(1, 2),
+(2, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `views`
+--
+ALTER TABLE `views`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `views`
+--
+ALTER TABLE `views`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
